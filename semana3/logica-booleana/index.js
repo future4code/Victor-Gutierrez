582b91114907050e51d7026b56665b43a4464355
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-04-27 19:04:42 -0400

    correção

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -27,7 +27,7 @@ console.log("e. ", typeof resultado)
 /* e.  boolean */
 
 
-let respostas ={ 
+let respostas = {
     a: " Arrays são varriáveis especiais que guardam mais de um valor ",
     b: " [0] ",
     c: "Se determina pelo número de valores dentro dele e se acessa pelo método ´.lenght´"
@@ -75,31 +75,38 @@ console.log('VII. ', resultadoC)
 /* VII.  1 */
 
 /* RESPOSTA A */
- 
 
+/* 
 let paramF = 77
-function converterFahren(paramF){
+let kelvin = (paramF - 32) * 5 / 9 + 273.15
+
+function converterFahren(paramF) {
     let kelvin = (paramF - 32) * 5 / 9 + 273.15;
     let celsius = Math.round(kelvin - 273);
     return `${paramF}ºF é igual a ${celsius}ºC e é igual a ${kelvin}K`
- } 
+} */
 
-console.log("RESPOSTA A => " + converterFahren(paramF))
+let paramF = 77;
+let kelvin = (paramF - 32) * 5 / 9 + 273.15;
 
-
+console.log("RESPOSTA A => " + kelvin)
 
 /* RESPOSTA DA D */
-let paramC = prompt("Insira qualquer valor em graus Celsius para converter")
+/* let paramC = prompt("Insira qualquer valor em graus Celsius para converter")
 
-function converterCelsius(paramC){
+function converterCelsius(paramC) {
     let celsius = paramC
     let fahren = celsius * 9 / 5 + 32
     let kelvin = (fahren - 32) * 5 / 9 + 273.15
-    return ` ${paramC}ºC em Fahrenheit é ${fahren}ºF e em Kelvin é ${kelvin}K`
-}
 
-console.log("RESPOSTA B D e C => " + converterCelsius(paramC))
+    return ` ${paramC}ºC em Fahrenheit é ${fahren}ºF e em Kelvin é ${kelvin}K` }*/
 
+let paramC = prompt("Insira qualquer valor em graus Celsius para converter");
+let celsius = paramC;
+let fahren = celsius * 9 / 5 + 32;
+let kelvin2 = (fahren - 32) * 5 / 9 + 273.15;
+
+console.log("RESPOSTA B D e C => " + `${paramC}ºC é igual a ${fahren}ºF e ${kelvin2}K`)
 
 /* RESPOSTA DA D */
 
@@ -111,79 +118,53 @@ let ask5 = prompt("Você gosta de entrevistas?")
 
 console.log("Suas respostas: \n " + ask1 + "\n", ask2 + "\n", ask3 + "\n", ask4 + "\n", ask5)
 
-
 /* Kilowatter */
 
 let kilowattCasa = 280 * 0.05;
 console.log("R$" + kilowattCasa);
 
-let askKw = prompt("Quantos quilowatts você gastou no último mês?")
+let askKw = prompt("Quantos quilowatts você gastou no último mês?");
+let kwPrice = askKw * 0.05;
+let discount = kwPrice - (kwPrice * 0.15);
 
-function killowatter(askKw){
-    let wattage = askKw * 0.05;
-    let discount = wattage * 0.15;
-    return `Você pagará: R$ ${wattage} \n e com um desconto de 15% pagaria: R$ ${discount}`
-}
-
-console.log(killowatter(askKw))
-
+console.log(`Você pagará: R$ ${kwPrice} \n e com um desconto de 15% pagaria: R$ ${discount}`)
 
 /* LIBRA */
 
 let libra = 20
-function convertLibra(libra){
-    return libra + " libras" + " equivalem a  " + Math.floor(libra * 0.453592) + "kg aproximadamente. "
-}
+console.log(libra + " libras" + " equivalem a  " + Math.floor(libra * 0.453592) + "kg aproximadamente. ")
 
-console.log(convertLibra(libra))
 /* LIBRA */
 
 /* ONÇA */
 
-
-
 let onca = 10.5
-function convertOnca(onca) {
-    return onca + " onças" + " equivalem a " + Math.floor(onca * 0.0283495) + "kg aproximadamente. "
-}
-
-console.log(convertOnca(onca))
+console.log(onca + " onças" + " equivalem a " + Math.floor(onca * 0.0283495) + "kg aproximadamente. ")
 
 /* ONÇA */
 /* MILHA */
 
 let milha = 100
-function convertMilha(milha) {
-    return milha + " milhas" + " equivalem a " + Math.floor(milha * 1.60934) + "km aproximadamente. "
-}
-console.log(convertMilha(milha))
+console.log(milha + " milhas" + " equivalem a " + Math.floor(milha * 1.60934) + "km aproximadamente. ")
 
 /* MILHA */
 /* FEETS */
 
 let feets = 50
-function convertFeets(feets) {
-    return feets + " ft" + " equivalem a " + Math.floor(feets * 0.3048) + "m aproximadamente. "
-}
-console.log(convertFeets(feets))
-
+console.log(feets + " ft" + " equivalem a " + Math.floor(feets * 0.3048) + "m aproximadamente. ")
 
 /* FEETS */
 /* GALÕES */
 
 let gal = 103.56
-function convertGal(gal) {
-    return gal + " gal" + " equivalem a " + Math.floor(gal * 3.78541) + " l aproximadamente. "
-}
-console.log(convertGal(gal))
+console.log(gal + " gal" + " equivalem a " + Math.floor(gal * 3.78541) + " l aproximadamente. ")
 
 /* GALÕES */
 /* XÍCARAS */
 
 let xic = 450
-function convertXic(xic) {
-    return xic + " xic" + " equivalem a " + Math.floor(xic * 4) + " l aproximadamente. "
-}
-console.log(convertXic(xic))
+console.log(xic + " xic" + " equivalem a " + Math.floor(xic * 4) + " l aproximadamente. ")
+
+
 
 /* XÍCARAS */
