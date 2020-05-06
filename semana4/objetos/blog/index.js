1e3a628859e90fa6f07b285a5023824ff20ccaaf
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-06 16:33:41 -0400

    fixes

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,40 +1,46 @@
+const objArr = [];
+let counter = -1;
+
 document.getElementById("modaltoggler").addEventListener("click", () => {
   document.getElementById("formmodal").classList.toggle("hide");
 });
-
-const objArr =  [];
 
 let newpostarea = document.getElementById("newpostarea");
 let sendButton = document.getElementById("sender");
 
 sendButton.addEventListener("click", () => {
-    event.preventDefault();
-    let postTitle = document.getElementById("postTitle").value;
-    let author = document.getElementById("nickname").value;
-    let content = document.getElementById("postContent").value;
-    let image = document.getElementById("photoUrl").value;
-    objArr.push(factory(postTitle, postTitle, author, content, image));
-    
-    render(objArr);
+  event.preventDefault();
+  counter++;
+  let postTitle = document.getElementById("postTitle").value;
+  let author = document.getElementById("nickname").value;
+  let content = document.getElementById("postContent").value;
+  let image = document.getElementById("photoUrl").value;
+  objArr.push(factory(postTitle, postTitle, author, content, image));
+
+  render(objArr);
 });
 
 function factory(objname, title, author, content, photoUrl) {
-    objname = {};
-    objname.title = title;
-    objname.creator = author;
-    objname.text = content;
-    objname.imageUrl = photoUrl;
-    return objname;
+  objname = {};
+  objname.title = title;
+  objname.creator = author;
+  objname.text = content;
+  objname.imageUrl = photoUrl;
+  return objname;
 }
 
 function render(objArr) {
-        
+  document.getElementById("formmodal").classList.toggle("hide");
   newpostarea.innerHTML += `<div class="postWrapper">
-                          <h2>${objArr[0].creator}</h2>
-                         <img src="${objArr[0].imageUrl}">
-                         <h3 class="title">${objArr[0].title}</h3>
-                         <p class="postsay">${objArr[0].text}</p>
-                            </div>`;
+    <h3>${objArr[counter].creator}</h3>
+    <img src="${objArr[counter].imageUrl}">
+    <h3 class="title">${objArr[counter].title}</h3>
+    <p class="postsay">${objArr[counter].text}</p>
+    </div>`;
 }
 
+postTitle = "";
+author = "";
+content = "";
+image = "";
 console.log(objArr);
