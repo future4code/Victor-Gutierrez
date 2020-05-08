a42bd010e378037d62800c47048fcb150de3a2ec
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-08 10:38:12 -0400

    exe5

--- semana4/lista-de-exercicios/scripts.js
+++ semana4/lista-de-exercicios/scripts.js
@@ -1,65 +1,118 @@
 /* INTERPRETAÇÃO DE CÓDIGO 1 ------------------------------------------------------------------------------ */
-console.log("INTERPRETAÇÃO DE CÓDIGO  -----------------------------------------------")
+console.log(
+  "INTERPRETAÇÃO DE CÓDIGO  -----------------------------------------------"
+);
 /* INTERPRETAÇÃO DE CÓDIGO  */
 /* 1 - A função recebe um argumento  transforma ele em número e retorna esse mesmo valor multiplicado pela cotação do dólar que o usuário inserir no console ele imptimre cotação * 100 \n */
-console.log("1 - A função recebe um argumento  transforma ele em número e retorna esse mesmo valor multiplicado pela cotação do dólar que o usuário inserir no console ele imptimre cotação * 100 \n")
+console.log(
+  "1 - A função recebe um argumento  transforma ele em número e retorna esse mesmo valor multiplicado pela cotação do dólar que o usuário inserir no console ele imptimre cotação * 100 \n"
+);
 /* 2 - a função recebe um tipo de investimento e um valor em o primeiro argumento passa por um switch e é multiplicado pelo segundo dependendo do tipo informado no final ela retorna o valor final desse cálcuco" */
-console.log("2 - a função recebe um tipo de investimento e um valor em o primeiro argumento passa por um switch e é multiplicado pelo segundo dependendo do tipo informado no final ela retorna o valor final desse cálcuco\n")
+console.log(
+  "2 - a função recebe um tipo de investimento e um valor em o primeiro argumento passa por um switch e é multiplicado pelo segundo dependendo do tipo informado no final ela retorna o valor final desse cálcuco\n"
+);
 /* 3 - A função percorre os array com loop e imprime os numeros pares em uma e os ímpares em outra no console foi impresso o tamanho das 3 arrays")" */
-console.log("3 - a A função percorre os array com loop e imprime os numeros pares em uma e os ímpares em outra no console foi impresso o tamanho das 3 arrays\n")
+console.log(
+  "3 - a A função percorre os array com loop e imprime os numeros pares em uma e os ímpares em outra no console foi impresso o tamanho das 3 arrays\n"
+);
 /* 4 -  o loop percore a array números e e reatribui numero1 e numero2 com os valores que passam nas condicionais, isso tudo em várias rodadas de loop")" */
-console.log("4 - o loop percore a array números e e reatribui numero1 e numero2 com os valores que passam nas condicionais, isso tudo em várias rodadas de loop \n")
-/* INTERPRETAÇÃO DE CÓDIGO  */ /* INTERPRETAÇÃO DE CÓDIGO  */
-
+console.log(
+  "4 - o loop percore a array números e e reatribui numero1 e numero2 com os valores que passam nas condicionais, isso tudo em várias rodadas de loop \n"
+);
+/* INTERPRETAÇÃO DE CÓDIGO  */
+/* INTERPRETAÇÃO DE CÓDIGO  */
 
 /* - Recomendamos que cada exercício esteja dentro de uma função.
 Lembre-se de comentar qual é o exercício OU deixe isso bem claro no nome da função. */
 
-
-
 /* LOGICCA DE PROGRAMAÇAO */
-console.log("LOGICCA DE PROGRAMAÇAO ----------------------------------------------- \n")
+console.log(
+  "LOGICCA DE PROGRAMAÇAO ----------------------------------------------- \n"
+);
 
-console.log("Exercício 1: com for, map, forEach")
+console.log("Exercício 1: com for, map, forEach");
 
-let baseArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+let baseArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 function iterateArray(array) {
-    let firstIteration = []
-    let secondIteration = []
-    let thirdIteration = []
-    for (item of array) {
-        firstIteration.push(item)
-    }
-/* ------------------------------- */
+  let firstIteration = [];
+  let secondIteration = [];
+  let thirdIteration = [];
+  for (item of array) {
+    firstIteration.push(item);
+  }
+  /* ------------------------------- */
 
-    array.map((index) => {
-        secondIteration.push(index)
-    })
-/* ------------------------------- */
-    array.forEach(element => {
-        thirdIteration.push(element)
-    });
+  array.map((index) => {
+    secondIteration.push(index);
+  });
+  /* ------------------------------- */
+  array.forEach((element) => {
+    thirdIteration.push(element);
+  });
 
-    console.log(`1: ${firstIteration} \n 2: ${secondIteration} \n  3: ${thirdIteration}`)
+  console.log(
+    `1: ${firstIteration} \n 2: ${secondIteration} \n  3: ${thirdIteration}\n\n`
+  );
 }
 
-iterateArray(baseArray)
-
+iterateArray(baseArray);
 
 /* ------------------------------------------------------------------------------ */
-console.log("Exercício 2 ------------------------------------------------------------------")
+console.log(
+  "Exercício 2 ------------------------------------------------------------------"
+);
 
-console.log("a) false \nb)false \nc) true \nd)true \ne)true ")
+console.log("a) false \nb)false \nc) true \nd)true \ne)true \n\n");
 
 /* "a) false b)false c) true d)true e)true */
 
+console.log(
+  "Exercício 3 ------------------------------------------------------------------"
+);
+console.log(
+  "Não funciona e gera um loop infinito, corrigindo o código: checkdocument\n\n"
+);
 
-console.log("Exercício 3 ------------------------------------------------------------------")
+/* Não funciona e gera um loop infinito, corrigindo o código: */
 
-const quantidadeDeNumerosPares = []
-let i = 0
+function getEvenPlusOne(number) {
+  let nEvens = [];
 
-while (i <= quantidadeDeNumerosPares) {
-    console.log(i * 2)
+  for (i = 0; i < number; i++) {
+    if (i % 2 === 0) {
+      nEvens.push(i);
+    }
+    nEvens.push(nEvens[nEvens.length - 1] + 2);
+  }
+  console.log(nEvens + "\n\n");
 }
+
+getEvenPlusOne(5);
+
+console.log(
+  "Exercício 4 ------------------------------------------------------------------"
+);
+
+function checkTriangle(a, b, c) {
+  switch (true) {
+    case a === b && b === c:
+      console.log(`É um triângulo equilátero`);
+      break;
+    case a === b || b === c || c === a:
+      console.log(`É um triângulo isósceles`);
+      break;
+    case a !== b && b !== c && c !== a:
+      console.log(`É um triângulo escaleno`);
+      break;
+    default:
+      console.log("Esse triângulo é anormal");
+      break;
+  }
+}
+
+checkTriangle(2, 2, 10);
+
+console.log(
+    "Exercício 5 ------------------------------------------------------------------"
+);
