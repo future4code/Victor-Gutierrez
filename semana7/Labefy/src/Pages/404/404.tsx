b969ab2267538cc5140aa7d5ad0b906410a6c11d
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-06-08 07:52:03 -0400

    fix3

--- semana7/Labefy/src/Pages/404/404.tsx
+++ semana7/Labefy/src/Pages/404/404.tsx
@@ -1,40 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Lost from '../../Assets/404.png';
-import styled from 'styled-components';
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100vh;
-    background-color: #f7f7f7;
-    background-position: center;
-    background-repeat: no-repeat;
-    text-align: center;
-    justify-content: space-between;
-
-    a {
-        text-decoration: none;
-        color: inherit;
-        color: black;
-        font-weight: bold;
-        position: relative;
-        bottom: 250px;
-        font-size: 32px;
-    }
-    a:hover {
-        color: #59caf5;
-    }
-
-    h1 {
-        margin-top: 50px;
-        color: black;
-    }
-    img {
-        margin-bottom: 250px;
-    }
-`;
+import { Container } from './404styles';
 
 export default function LostPage() {
     return (
