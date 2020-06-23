48c0b92988e6599fc6c42c98e0863e047d1700f5
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-06-22 20:19:55 -0400

    Improvement: Removal of not-so-sure optimizations and detailing of context funcionality

--- semana8/astromatch/src/Context/profileChangeListener_Context.tsx
+++ semana8/astromatch/src/Context/profileChangeListener_Context.tsx
@@ -6,6 +6,8 @@ export const profileChangeListener = createContext<
     Context_profileChangeListener
 >({} as Context_profileChangeListener);
 
+// Esse context foi criado para sincronizar as atualizações do CardChooser.tsx com as do Matches.tsx
+
 export function Provider({ children }) {
     const [generatedNumber, setContext] = useState<number>(1);
 
