cd04f62392841e3842728f7cb5e2883d692898a1
Victor <victorgutierrezgomes@gmail.com>
2020-07-27 19:59:48 -0400

    Fix ( ex6 ) : split-code

--- semana12/src/ex6/ex6.ts
+++ semana12/src/ex6/ex6.ts
@@ -1,24 +1,15 @@
-type PrintYearAndAgeType = (year: number, age?: 'DC' | 'AC') => any;
+import { PrintYearAndAgeType, Ages } from './ex6.types';
 
 const printYearAndAge: PrintYearAndAgeType = (year, age = 'DC') => {
-  enum Ages {
-    PREHISTORIA = 'Pré-historia',
-    ANTIGA = 'Idade Antiga',
-    MEDIA = 'Idade Média',
-    MODERNA = 'Idade Moderna',
-    ATUAL = 'Idade Contemporânea',
-    FUTURO = 'uma idade que ainda não sabemos',
-  }
-
   const outputString = `${year} ${age} pertence à`;
 
-  if (year >= 4000 && year < 100000) {
+  if (year >= 4001 && year < 100000) {
     return age === 'DC'
       ? `${outputString} ${Ages.FUTURO}`
       : `${outputString} ${Ages.PREHISTORIA}`;
   } else if (year >= 0 && year < 475) {
     return `${outputString} ${Ages.ANTIGA}`;
-  } else if (year >= 475 && year < 1451) {
+  } else if (year >= 475 && year <= 1451) {
     return age === 'DC'
       ? `${outputString} ${Ages.MEDIA}`
       : `${outputString} ${Ages.ANTIGA}`;
