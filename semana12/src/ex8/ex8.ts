0325124371fc80cfa10ecc5f5393966b5230ddf0
Victor <victorgutierrezgomes@gmail.com>
2020-07-27 21:14:11 -0400

    Chore ( ex8 ) : implement ex8

---
+++ semana12/src/ex8/ex8.ts
@@ -0,0 +1,48 @@
+import { RestaurantManagerInterface, Product, SoldProduct } from './ex8.types';
+
+class RestaurantManager implements RestaurantManagerInterface {
+  public products: Product[];
+  public sold: SoldProduct[];
+
+  constructor(initialArrayOfProducts: Product[]) {
+    this.products = initialArrayOfProducts;
+    this.sold = [];
+  }
+
+  addNewProduct(product: Product) {
+    this.products.push(product);
+  }
+
+  seePriceOfProductInList(productName: string) {
+    const index = this.products.findIndex(
+      (product) => product.name === productName
+    );
+
+    return index > -1
+      ? this.products[index].sellPrice
+      : 'Produto não encontrado';
+  }
+
+  sellProducts(productName: string) {
+    const index = this.products.findIndex(
+      (product) => product.name === productName
+    );
+    this.sold.push({ ...this.products[index], sellTime: Date.now() });
+  }
+
+  get restaurantProfit() {
+    return (
+      this.sold.reduce((previous, current) => previous + current.sellPrice, 0) -
+      this.products.reduce((previous, current) => previous + current.cost, 0)
+    );
+  }
+
+  get seeAllProducts() {
+    return this.products;
+  }
+  get seeAllSoldProducts() {
+    return this.sold;
+  }
+}
+
+export default RestaurantManager;
