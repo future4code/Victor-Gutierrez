d7015672b04746a0f6bd0cace830ba24081b070f
Victor <victorgutierrezgomes@gmail.com>
2020-07-30 13:19:06 -0400

    Chore ( labenews-controller ) : set types

---
+++ semana12/aula47/src/Exercicios/Labenews.types.ts
@@ -0,0 +1,25 @@
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface News {
+  title: string;
+  content: string;
+  date: number;
+}
+
+interface ILabenewsController {
+  getSubscribers(): Promise<User[]>;
+  createNews(content: Omit<News, 'date'>): Promise<void>;
+  createNewsAndNotifyUsers(
+    content: Omit<News, 'date'>,
+    notificationMessage: string
+  ): Promise<void>;
+  notifyAllUsers(target: User[], message: string): Promise<void>;
+  createUser(data: Omit<User, 'id'>): Promise<void>;
+  getAllNotifications(): Promise<any>;
+}
+
+export { User, News, ILabenewsController };
