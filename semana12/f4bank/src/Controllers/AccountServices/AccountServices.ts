85161c37c19c349f8e0a7448929e4b33fd7d3db8
Victor <victorgutierrezgomes@gmail.com>
2020-07-31 15:14:54 -0400

    Feature ( controllers ) : implement checkBalance

--- semana12/f4bank/src/Controllers/AccountServices/AccountServices.ts
+++ semana12/f4bank/src/Controllers/AccountServices/AccountServices.ts
@@ -2,22 +2,40 @@ import { IUserAccount } from "../../Types/Types";
 import { validateCPF } from "../../Helpers/CPFValidator";
 import { validateAge } from "../../Helpers/AgeValidator";
 import { UserAccount } from "../../Models/UserAccount";
+import AccountRepository from "../../Repositories/AccountRepository";
 
 class AccountService {
-    async createAccount(
-        accountData: Omit<IUserAccount, "balance" | "history">
-    ): Promise<void> {
+    async createAccount({
+        name,
+        CPF,
+        birthdate,
+    }: Omit<IUserAccount, "balance" | "history">): Promise<void> {
         try {
-            await validateCPF(accountData.CPF);
-            await validateAge(accountData.birthdate);
+            await validateCPF(CPF);
+            await validateAge(birthdate);
 
             new UserAccount({
-                name: accountData.name,
-                CPF: accountData.CPF,
-                birthdate: accountData.birthdate,
+                name: name,
+                CPF: CPF,
+                birthdate: birthdate,
             });
         } catch (error) {
             throw new Error(error);
         }
     }
+
+    async checkBalance({ CPF }: Pick<IUserAccount, "CPF">) {
+        try {
+            await validateCPF(CPF);
+            await AccountRepository.queryDatabaseForUser({ CPF: CPF });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
 }
+
+export default new AccountService();
+
+const aaa = new AccountService();
+
+aaa.checkBalance({ CPF: "111.111.111-88" });
