30b93b8bdbcd18802591386f8841fbecfa57e921
Victor <victorgutierrezgomes@gmail.com>
2020-08-03 15:10:11 -0400

    Chore: implement Bank

--- semana13/aula48/src/Entities/Bank.ts
+++ semana13/aula48/src/Entities/Bank.ts
@@ -1,26 +1,46 @@
-import JSONFileManager
+import { IAccount } from "../Types";
+import JSONFileManager from "./JSONFileManager";
+import UserAccount from "./UserAccount";
 
 class Bank {
-    private accounts: Account[];
-    private fileManager: JSONFileManager;
+    private readonly accounts: IAccount[];
 
-    constructor(accounts: UserAccount[], fileManager: JSONFileManager) {
+    constructor(accounts: IAccount[]) {
         this.accounts = accounts;
-        this.fileManager = fileManager;
     }
 
-    public createAccount(userAccount: UserAccount): void {
-        //lógica de criar conta aqui
+    public createAccount({
+        cpf,
+        name,
+        age,
+    }: Pick<IAccount, "cpf" | "name" | "age">): void {
+        if (age > 17 && cpf.match(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)) {
+            JSONFileManager.writeObjectToFile(new UserAccount(cpf, name, age));
+        } else {
+            console.log("Cheque os dados");
+        }
     }
 
-    public getAllAccounts(): UserAccount[] {
-        //lógica de pegar todas as contas aqui
+    public getAllAccounts(): IAccount[] {
+        const db = JSONFileManager.getObjectFromFile();
+
+        return db;
     }
 
     public getAccountByCpfAndName(
         cpf: string,
         name: string
-    ): UserAccount | undefined {
-        // lógica de pegar conta pelo CPF e Nome aqui
+    ): IAccount | undefined {
+        const data = this.accounts.find(
+            (account: IAccount) => account.name === name && account.cpf === cpf
+        );
+
+        return data;
     }
 }
+
+export default new Bank(JSONFileManager.getObjectFromFile());
+
+const bd = new Bank(JSONFileManager.getObjectFromFile());
+
+bd.createAccount({ cpf: "111.111.111-73", name: "gustaaaaaaaavbo", age: 22 });
