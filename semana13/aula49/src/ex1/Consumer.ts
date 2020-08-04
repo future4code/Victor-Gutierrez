05399cc324f1b68d243d94ee3565302e4c596a89
Victor <victorgutierrezgomes@gmail.com>
2020-08-04 14:29:35 -0400

    chore: implement Consumer

---
+++ semana13/aula49/src/ex1/Consumer.ts
@@ -0,0 +1,34 @@
+import User from "./User";
+
+class Customer extends User {
+    public readonly purchaseTotal: number;
+    private creditCard: string;
+
+    constructor(
+        id: string,
+        email: string,
+        name: string,
+        password: string,
+        creditCard: string
+    ) {
+        super(id, email, name, password);
+        console.log("Chamando o construtor da classe Customer");
+        this.creditCard = creditCard;
+        this.purchaseTotal = 0;
+    }
+
+    public getCreditCard(): string {
+        return this.creditCard;
+    }
+}
+
+const costumer = new Customer(
+    "?",
+    "aaa@mail.com",
+    "asdasd",
+    "123123123123",
+    "213231231231231223"
+);
+
+//ex2 a) 1 b) 2 por que Costumer é descendente de User
+
