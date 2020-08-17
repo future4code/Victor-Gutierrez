7bc9da2116e197346f2cdbc2ff885965eb71f9e9
Victor <victorgutierrezgomes@gmail.com>
2020-08-17 14:48:42 -0400

    Chore: broader errorhandle operation

--- semana15/aula1/src/Helpers/errorHandler.ts
+++ semana15/aula1/src/Helpers/errorHandler.ts
@@ -1,6 +1,10 @@
 import { Response } from 'express';
 
-const errorHandler = (error: Error, res: Response, statusCode = 400) => {
+const errorHandler = (
+      error: Error | string,
+      res: Response,
+      statusCode = 400
+) => {
       return res.status(statusCode).json({ message: error });
 };
 
