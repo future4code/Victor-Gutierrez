a75b87d40242900e717a5fc7026498e5318d852a
Victor <victorgutierrezgomes@gmail.com>
2020-08-16 20:52:56 -0400

    Feature: implement dateparser helper

---
+++ semana14/todolist/src/Helpers/dateSQLParser.ts
@@ -0,0 +1,8 @@
+const dateSQLParser = (date: string) => {
+      if (date.match(/^\d{1,2}\/\d{1,2}\/\d{4}$/))
+            throw 'Formato de data inválido. Use DD/MM/YYYY';
+
+      return date.split('/').reverse().join('/');
+};
+
+export default dateSQLParser;
