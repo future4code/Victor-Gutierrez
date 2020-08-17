ab7d21057cded14f6556e0454889622ab7228a9c
Victor <victorgutierrezgomes@gmail.com>
2020-08-17 13:56:48 -0400

    Chore: implement Authenticator service

---
+++ semana15/aula1/src/Services/authentication.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+
+export class Authenticator {
+      private static EXPIRES_IN = '1min';
+      public generateToken(input: string): string {
+            const token = jwt.sign(
+                  {
+                        id: input,
+                  },
+                  process.env.JWT_KEY as string,
+                  {
+                        expiresIn: Authenticator.EXPIRES_IN,
+                  }
+            );
+            return token;
+      }
+}
